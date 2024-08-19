58c9b4d620e11aaf597c598aaf298dd23d386754
vishal <kannavish329@gmail.com>
2024-08-19 16:56:21 +0530

    chore: module integration isn't working

--- post/post.go
+++ post/post.go
@@ -8,17 +8,18 @@ import (
 
 // HandlerOptions are the options required for constructing a default post handler
 type HandlerOptions struct {
-	AccountKeeper  ante.AccountKeeper
-	BankKeeper     cheqdante.BankKeeper
-	FeegrantKeeper ante.FeegrantKeeper
-	DidKeeper      cheqdante.DidKeeper
-	ResourceKeeper cheqdante.ResourceKeeper
+	AccountKeeper   ante.AccountKeeper
+	BankKeeper      cheqdante.BankKeeper
+	FeegrantKeeper  ante.FeegrantKeeper
+	DidKeeper       cheqdante.DidKeeper
+	ResourceKeeper  cheqdante.ResourceKeeper
+	FeeMarketKeeoer cheqdante.FeeMarketKeeper
 }
 
 // NewPostHandler returns a default post handler
 func NewPostHandler(options HandlerOptions) (sdk.PostHandler, error) {
 	postDecorators := []sdk.PostDecorator{
-		NewTaxDecorator(options.AccountKeeper, options.BankKeeper, options.FeegrantKeeper, options.DidKeeper, options.ResourceKeeper),
+		NewTaxDecorator(options.AccountKeeper, options.BankKeeper, options.FeegrantKeeper, options.DidKeeper, options.ResourceKeeper, options.FeeMarketKeeoer),
 	}
 	return sdk.ChainPostDecorators(postDecorators...), nil
 }
